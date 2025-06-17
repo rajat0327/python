dbf4e59991c69736264623fbc9c566a6275369fc
rajat0327 <rajat.pes2013@gmail.com>
2025-06-17 09:30:35 +0000

    first commit

---
+++ str-concact.py
@@ -0,0 +1,8 @@
+
+print("20 days are " + str(50) + " mintues")
+print(f"20 days are {50} minutes")
+
+to_seconds = 24 * 60 * 60
+name_of_unit = "seconds"
+print(f"20 days are {20 * to_seconds} {name_of_unit}")
+print(f"35 days are {35 * to_seconds} {name_of_unit}")
